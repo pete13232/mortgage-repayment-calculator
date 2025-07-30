0158a2208209ee72916bfbc4048aca73d3e9dda1
Pete <peetawit.vong@mail.kmutt.ac.th>
2025-07-30 20:41:27 +0700

    UPDATE | Change result badge

--- src/component/Result.jsx
+++ src/component/Result.jsx
@@ -1,27 +1,44 @@
+import { EmptyIcon } from "./EmptyIcon"
+
 export const Result = ({ monthlyPayment, totalPayment }) => {
-    return (
-        <div className="result-container has-result">
-            <div className="result-header has-result">
-                <h2>Your results</h2>
-                <p className="result-description">Your results are shown below based on the information you provided.
-                    To adjust the results, edit the form and click “calculate repayments” again.</p>
-            </div>
-            <div className="result-body has-result">
-                <div className="result-body__top">
-                    <h3>Your monthly repayments</h3>
-                    <p>{monthlyPayment.toLocaleString('en-GB', {
-                        style: 'currency',
-                        currency: 'GBP'
-                    })}</p>
+    return (<>
+        {monthlyPayment && totalPayment
+            ?
+            <div className="result-container has-result">
+                <div className="result-header has-result">
+                    <h2>Your results</h2>
+                    <p className="result-description">Your results are shown below based on the information you provided.
+                        To adjust the results, edit the form and click “calculate repayments” again.</p>
                 </div>
-                <div className="result-body__bottom">
-                    <h3>Total you'll repay over the term</h3>
-                    <p>{totalPayment.toLocaleString('en-GB', {
-                        style: 'currency',
-                        currency: 'GBP'
-                    })}</p>
+                <div className="result-body has-result">
+                    <div className="result-body__top">
+                        <h3>Your monthly repayments</h3>
+                        <p>{monthlyPayment.toLocaleString('en-GB', {
+                            style: 'currency',
+                            currency: 'GBP'
+                        })}</p>
+                    </div>
+                    <div className="result-body__bottom">
+                        <h3>Total you'll repay over the term</h3>
+                        <p>{totalPayment.toLocaleString('en-GB', {
+                            style: 'currency',
+                            currency: 'GBP'
+                        })}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            :
+            <div className="result-container">
+                <EmptyIcon />
+                <div className="result-header">
+                    <h2>Results shown here</h2>
+                    <p className="result-description"> Complete the form and click “calculate repayments” to see what
+                        your monthly repayments would be.</p>
+                </div>
+            </div>}
+
+
+
+    </>
     )
 }
